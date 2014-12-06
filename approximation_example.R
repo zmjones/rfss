52a9ca2abbb1b714affc13c330722bb1b9ab106c
Zachary M. Jones <zmj@zmjones.com>
2014-12-06 01:00:23 -0500

    adds function approximation figure and code

---
+++ approximation_example.R
@@ -0,0 +1,24 @@
+set.seed(1988)
+
+library(party)
+library(ggplot2)
+library(reshape2)
+
+n <- 200
+x <- runif(n, -4, 4)
+y <- sin(x)
+df <- data.frame(x, y)
+
+fit_tree <- ctree(y ~ x, df)
+df$tree <- as.numeric(predict(fit_tree))
+
+fit_forest <- cforest(y ~ x, df, control = cforest_unbiased(mtry = 1, ntree = 1000))
+df$forest <- as.numeric(predict(fit_forest))
+df <- melt(df, id.vars = c("x", "y"))
+
+p <- ggplot(df, aes(x, y))
+p <- p + facet_wrap( ~ variable)
+p <- p + geom_point(alpha = .5)
+p <- p + geom_line(aes(x, value), colour = "blue")
+p <- p + theme_bw()
+ggsave("approximation_example.png", p, width = 8, height = 4)
